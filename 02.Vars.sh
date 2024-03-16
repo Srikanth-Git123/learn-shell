dbd4f36418bc62a1a32102754ef2eef557a17000
Srikanth Palakuri <palakuri.srikanth23@gmail.com>
2024-03-16 16:16:10 +0530

    command and arithmetic substitution in variable

--- 02.Vars.sh
+++ 02.Vars.sh
@@ -4,5 +4,17 @@ a=10
 #Variable with a special character data (Space is also a special characte), Then double quote it
 b="Hello World"
 
+
 echo $a
 echo $b
+
+# Substitution
+# Command Sub
+# A command output if we want in a variable then command sub
+# syntax : Var=$(command)
+x=$(uptime)
+
+# Arithmetic Sub
+# A Arithmetic expression solved and output to a variable then arithmetic sub
+y=$((2+3))
+z=$((2+3+/8*2))
